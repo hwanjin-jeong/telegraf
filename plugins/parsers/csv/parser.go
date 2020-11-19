6bcdfdb03f26ec8264e5466a6611842ca4d990f1
Stephanie Engel <sengel@influxdata.com>
2020-11-19 14:06:09 -0600

    keep field name as is for csv timestamp column

--- plugins/parsers/csv/parser.go
+++ plugins/parsers/csv/parser.go
@@ -238,8 +238,11 @@ outer:
 				continue
 			}
 
-			// attempt type conversions
-			if iValue, err := strconv.ParseInt(value, 10, 64); err == nil {
+			// If the field name is the timestamp column, then keep field name as is.
+			// Else, attempt implicit type conversions.
+			if fieldName == p.TimestampColumn {
+				recordFields[fieldName] = value
+			} else if iValue, err := strconv.ParseInt(value, 10, 64); err == nil {
 				recordFields[fieldName] = iValue
 			} else if fValue, err := strconv.ParseFloat(value, 64); err == nil {
 				recordFields[fieldName] = fValue
